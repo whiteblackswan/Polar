8a500c28887bd48ce303edd0633112d33dc868f4
Sergey Lebed <swansergey7@gmail.com>
2022-09-21 16:42:01 +0300

    jQuery-script for tabs

--- src/js/script.js
+++ src/js/script.js
@@ -8,10 +8,18 @@ $(document).ready(function () {
 			{
 				breakpoint: 992,
 				settings: {
-					dots: true,
+					dots: false,
 					arrows: false,
 				}
 			}
 		]
 	});
+
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+
+	$()
 });
