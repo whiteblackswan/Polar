73e662f97356bcfe85acd259946f3a24ffbb7ca3
Sergey Lebed <swansergey7@gmail.com>
2022-10-11 15:37:32 +0300

    add new section

--- src/js/script.js
+++ src/js/script.js
@@ -2,38 +2,16 @@ $(document).ready(function () {
 	$('.carousel__inner').slick({
 		speed: 1200,
 		adaptiveHeight: true,
-		prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/left.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/right.png"></button>',
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
 		responsive: [
 			{
-				breakpoint: 1024,
+				breakpoint: 992,
 				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					infinite: true,
 					dots: true,
-					arrows: true
-				}
-			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					arrows: false,
-					dots: true
-
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					dots: true
+					arrows: false
 				}
 			}
-
 		]
 	});
 
@@ -56,7 +34,7 @@ $(document).ready(function () {
 	toggleSlide('.catalog-item__link');
 	toggleSlide('.catalog-item__back');
 
-	//Modal
+	// Modal
 
 	$('[data-modal=consultation]').on('click', function () {
 		$('.overlay, #consultation').fadeIn('slow');
@@ -102,5 +80,44 @@ $(document).ready(function () {
 	validateForms('#consultation-form');
 	validateForms('#consultation form');
 	validateForms('#order form');
+
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	$('form').submit(function (e) {
+		e.preventDefault();
+
+		if (!$(this).valid()) {
+			return;
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+
+	// Smooth scroll and pageup
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href=#up]").click(function () {
+		const _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" }, 800);
+		return false;
+	});
+
 
 });
